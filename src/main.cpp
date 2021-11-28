b4bd34ce48b9f4b89d01ecc48925f19b40681cc3
root <root@localhost>
2021-11-28 14:56:32 +0530

    auto publish 1638091592102

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,12 @@
 
 using namespace std;
 
-int main(/*int argc, char** argv*/){
-    /*cout << "Hello world" << endl;*/
+int main(){
     
-    /*int age;
-    cin>>age;
-    if (age>=18){ cout<<"you are selected";}
-    else{cout<<"not";}*/
     
-    int index = (int) 34
+    
+    
+    int index =(int) 0
     while(index<34){
     cout<<"hello"<<index<<endl;
     index = index + 1;}
